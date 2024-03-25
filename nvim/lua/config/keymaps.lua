a681e19d675b0e9cca476c4c55bc1e66d66767a1
spojskic <pojskicsafet@gmail.com>
2024-03-25 09:34:41 +0100

    Add toggle terminal plugin

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,5 @@ vim.keymap.set(
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "Resume" }
 )
+
+vim.keymap.set("n", "<leader>bT", ":ToggleTerm direction=horizontal <CR>", { desc = "Open horizontal terminal split" })
